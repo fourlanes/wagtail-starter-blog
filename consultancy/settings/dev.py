5d59525712e4e3d41376bfae81f804b4b0258f2e
Edwin P. Magezi <edwinm_p@yahoo.com>
2023-03-02 09:04:13 +0300

    Initial commit

---
+++ consultancy/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-hq(!b9@#*4ya&km-owj_n2_=@)ou2d#ut@g-gv14zg4n$@+&il"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
