c52a60e8328df99c3e028f002dbb3aa4a98692aa
Edwin P. Magezi <edwinm_p@yahoo.com>
2023-03-02 20:29:26 +0300

    Add support for env configured database (postgresql)

--- consultancy/settings/base.py
+++ consultancy/settings/base.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import dj_database_url
+from decouple import config
 
 PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 BASE_DIR = os.path.dirname(PROJECT_DIR)
@@ -85,10 +87,7 @@ WSGI_APPLICATION = "consultancy.wsgi.application"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-    }
+    "default": dj_database_url.config(default=config("DATABASE_URL"))
 }
 
 
