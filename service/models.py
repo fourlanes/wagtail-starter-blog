9695f7c33e4a1dc3daebad451f14467de34ccf96
Edwin P. Magezi <edwinm_p@yahoo.com>
2023-04-16 17:43:59 +0300

    Add content streamfield to service page

--- service/models.py
+++ service/models.py
@@ -1,9 +1,12 @@
 from django.db import models  # noqa: F401
 
 from wagtail.models import Page
+from wagtail.fields import StreamField
+from wagtail.admin.panels import FieldPanel
 
 from common.mixins import ContactUsFooterMixin, CustomMetadataPageMixin
 from common.utils import ContactUsFooterPanels
+from common.blocks import ContactUsBlock, HeroBlock
 
 
 class ServicePage(ContactUsFooterMixin, CustomMetadataPageMixin, Page):
@@ -14,6 +17,11 @@ class ServicePage(ContactUsFooterMixin, CustomMetadataPageMixin, Page):
     parent_page_type = ["home.HomePage"]
     subpage_type = ["service.ServicePage"]
 
+    content = StreamField(
+        [("hero", HeroBlock()), ("contact_us", ContactUsBlock())], use_json_field=True, null=True, blank=False
+    )
+
     content_panels = Page.content_panels + [
+        FieldPanel("content"),
         ContactUsFooterPanels(),
     ]
