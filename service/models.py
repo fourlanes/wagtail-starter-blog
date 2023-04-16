11cf0fa647a0696aba496a0d6c9e527bf59a29b3
Edwin P. Magezi <edwinm_p@yahoo.com>
2023-04-16 17:22:40 +0300

    Add ServicePage

--- service/models.py
+++ service/models.py
@@ -1,3 +1,19 @@
 from django.db import models  # noqa: F401
 
-# Create your models here.
+from wagtail.models import Page
+
+from common.mixins import ContactUsFooterMixin, CustomMetadataPageMixin
+from common.utils import ContactUsFooterPanels
+
+
+class ServicePage(ContactUsFooterMixin, CustomMetadataPageMixin, Page):
+    class Meta:
+        verbose_name = "Service Page"
+        verbose_name_plural = "Services Page"
+
+    parent_page_type = ["home.HomePage"]
+    subpage_type = ["service.ServicePage"]
+
+    content_panels = Page.content_panels + [
+        ContactUsFooterPanels(),
+    ]
