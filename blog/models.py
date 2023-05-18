c9a113657e5e414126bd56762a005c3807776cc1
Edwin P. Magezi <edwinm_p@yahoo.com>
2023-05-18 15:02:38 +0300

    Add work and blog pages

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,43 @@
 from django.db import models  # noqa: F401
 
-# Create your models here.
+from modelcluster.fields import ParentalKey
+from modelcluster.contrib.taggit import ClusterTaggableManager
+from taggit.models import TaggedItemBase
+
+from wagtail.models import Page
+from wagtail.admin.panels import FieldPanel
+
+from common.mixins import ContactUsFooterMixin, CustomMetadataPageMixin
+from common.utils import ContactUsFooterPanels, WagtailImageField
+
+
+class Topic(TaggedItemBase):
+    content_object = ParentalKey(Page, on_delete=models.CASCADE, related_name="page_topics")
+
+
+class BlogListing(ContactUsFooterMixin, CustomMetadataPageMixin, Page):
+    class Meta:
+        verbose_name = "Blog Listing"
+
+    parent_page_types = ["home.HomePage"]
+    subpage_types = ["blog.ArticlePage"]
+
+    content_panels = Page.content_panels + [
+        ContactUsFooterPanels(),
+    ]
+
+
+class ArticlePage(ContactUsFooterMixin, CustomMetadataPageMixin, Page):
+    class Meta:
+        verbose_name = "Article Page"
+
+    parent_page_types = ["blog.BlogListing", "work.WorkListing"]
+
+    hero_image = WagtailImageField(verbose_name="Hero Image")
+    topics = ClusterTaggableManager(through=Topic, blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel("hero_image"),
+        FieldPanel("topics"),
+        ContactUsFooterPanels(),
+    ]
--- work/models.py
+++ work/models.py
@@ -1,3 +1,18 @@
 from django.db import models  # noqa: F401
 
-# Create your models here.
+from wagtail.models import Page
+
+from common.mixins import ContactUsFooterMixin, CustomMetadataPageMixin
+from common.utils import ContactUsFooterPanels
+
+
+class WorkListing(ContactUsFooterMixin, CustomMetadataPageMixin, Page):
+    class Meta:
+        verbose_name = "Work Listing"
+
+    parent_page_types = ["home.HomePage"]
+    subpage_types = ["blog.ArticlePage"]
+
+    content_panels = Page.content_panels + [
+        ContactUsFooterPanels(),
+    ]
