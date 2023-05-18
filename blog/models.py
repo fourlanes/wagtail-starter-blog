5dcc7fa9d6a8e7f459e5de80c587802f1a84bdb1
Edwin P. Magezi <edwinm_p@yahoo.com>
2023-05-18 15:16:30 +0300

    Disable free tagging for topics

--- blog/models.py
+++ blog/models.py
@@ -2,16 +2,31 @@ from django.db import models  # noqa: F401
 
 from modelcluster.fields import ParentalKey
 from modelcluster.contrib.taggit import ClusterTaggableManager
-from taggit.models import TaggedItemBase
+from taggit.models import TagBase, TaggedItemBase
 
 from wagtail.models import Page
 from wagtail.admin.panels import FieldPanel
+from wagtail.snippets.models import register_snippet
 
 from common.mixins import ContactUsFooterMixin, CustomMetadataPageMixin
 from common.utils import ContactUsFooterPanels, WagtailImageField
 
 
+@register_snippet
+class ArticleTopic(TagBase):
+    free_tagging = False
+
+    panels = [
+        FieldPanel("name"),
+    ]
+
+    class Meta:
+        verbose_name = "Article Topic"
+        verbose_name_plural = "Article Topics"
+
+
 class Topic(TaggedItemBase):
+    tag = models.ForeignKey(ArticleTopic, related_name="tagged_article", on_delete=models.CASCADE)
     content_object = ParentalKey(Page, on_delete=models.CASCADE, related_name="page_topics")
 
 
