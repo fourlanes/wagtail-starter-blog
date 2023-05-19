563114f0e6c682c5f87078dbdb365305cde5eaaf
Edwin P. Magezi <edwinm_p@yahoo.com>
2023-05-19 15:31:10 +0300

    Show title and tags on work listing

--- work/models.py
+++ work/models.py
@@ -1,9 +1,11 @@
 from django.db import models  # noqa: F401
+from django.contrib.contenttypes.models import ContentType
 
 from wagtail.models import Page
 
 from common.mixins import ContactUsFooterMixin, CustomMetadataPageMixin
 from common.utils import ContactUsFooterPanels
+from blog.models import ArticlePage, ArticleTopic
 
 
 class WorkListing(ContactUsFooterMixin, CustomMetadataPageMixin, Page):
@@ -17,3 +19,15 @@ class WorkListing(ContactUsFooterMixin, CustomMetadataPageMixin, Page):
     content_panels = Page.content_panels + [
         ContactUsFooterPanels(),
     ]
+
+    def get_context(self, request):
+        context = super(WorkListing, self).get_context(request)
+
+        article_content_type = ContentType.objects.get_for_model(ArticlePage)
+        context["topics"] = (
+            ArticleTopic.objects.filter(tagged_article__content_object__content_type=article_content_type)
+            .distinct()
+            .order_by("name")
+        )
+
+        return context
