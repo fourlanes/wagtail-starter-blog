d13f506c7203ce2f46754c136765d442e9f458cb
Edwin P. Magezi <edwinm_p@yahoo.com>
2023-05-28 16:32:51 +0300

    Add sitemap to URL patterns

--- consultancy/urls.py
+++ consultancy/urls.py
@@ -1,11 +1,12 @@
 from django.conf import settings
-from django.urls import include, path
+from django.urls import include, path, re_path
 from django.contrib import admin
 from django.views.generic import TemplateView
 
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail import urls as wagtail_urls
 from wagtail.documents import urls as wagtaildocs_urls
+from wagtail.contrib.sitemaps.views import sitemap
 
 from search import views as search_views
 
@@ -14,6 +15,7 @@ urlpatterns = [
     path("admin/", include(wagtailadmin_urls)),
     path("documents/", include(wagtaildocs_urls)),
     path("search/", search_views.search, name="search"),
+    re_path(r"^sitemap\.xml$", sitemap),
 ]
 
 
