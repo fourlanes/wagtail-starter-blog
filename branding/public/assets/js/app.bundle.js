5ffa38bd431a1b7c8e34da930e027f2592216f30
Edwin P. Magezi <edwinm_p@yahoo.com>
2025-01-29 01:30:07 +0300

    Add images

--- branding/public/assets/js/app.bundle.js
+++ branding/public/assets/js/app.bundle.js
@@ -1,2 +1,2 @@
-!function(){"use strict";$((function(){console.log("ᕕ( ᐛ )ᕗ Running..."),console.log("ᕕ( ᐛ )ᕗ Globals...")}))}();
+!function(){"use strict";console.log("ᕕ( ᐛ )ᕗ Running..."),console.log("ᕕ( ᐛ )ᕗ Globals...")}();
 //# sourceMappingURL=app.bundle.js.map
